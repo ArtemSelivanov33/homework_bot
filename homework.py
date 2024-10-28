c59dc6913c9e66a4b6acc6951c539c1be8a946a4
Artem Selivanov <mr.anderson203033@gmail.com>
2024-10-28 20:59:09 +0300

    Исправил недочеты по PEP8

--- homework.py
+++ homework.py
@@ -41,11 +41,11 @@ def check_tokens():
         PRACTICUM_TOKEN,
         TELEGRAM_TOKEN,
         TELEGRAM_CHAT_ID
-        ]
+    ]
     missing_variable = [
         variable for variable in required_variables
         if os.getenv(variable) is None
-        ]
+    ]
     if missing_variable:
         logging.critical("Отсутствуют необходимые переменные окружения.")
         for variable in missing_variable:
@@ -55,7 +55,7 @@ def check_tokens():
 
 
 def send_message(bot, message):
-    """Отправляет сообщение в Telegram-чат."""    
+    """Отправляет сообщение в Telegram-чат."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logging.debug(f'Бот отправил сообщение "{message}"')
@@ -64,26 +64,25 @@ def send_message(bot, message):
 
 
 def get_api_answer(timestamp):
-    """Делает запрос к API и возвращает ответ в формате Python."""        
+    """Делает запрос к API и возвращает ответ в формате Python."""
     params = {'from_date': timestamp}
     try:
         response = requests.get(
-            url = ENDPOINT,
-            headers = HEADERS,
+            url=ENDPOINT,
+            headers=HEADERS,
             params=params
-            )
+        )
         if response.status_code != 200:
             logging.error(
                 f'Ошибка API: {response.status_code}, {response.text}'
-                )
+            )
             raise ConnectionError(f'Ошибка API: {response.status_code}')
-        
+
         return response.json()
 
     except requests.exceptions.RequestException as e:
         logging.error(f'Сетевая ошибка: {e}')
         raise  ConnectionError(f'Ошибка API: {response.status_code}')
-
 
 def check_response(response):
     """Проверяет ответ API на соответствие документации."""
@@ -93,28 +92,28 @@ def check_response(response):
         raise KeyError('В ответе отсутствует ключ "homeworks".')
     homeworks = response['homeworks']
     if not isinstance(homeworks, list):
-        raise TypeError('Значение ключа "homeworks" должно быть списком.')    
+        raise TypeError('Значение ключа "homeworks" должно быть списком.')
     return homeworks
 
 
 def parse_status(homework):
     """Извлекает статус проверки работы и формирует сообщение."""
     homework_name = homework.get('homework_name')
-    homework_status = homework.get('status')    
+    homework_status = homework.get('status') 
     if homework_name is None:
-        raise KeyError('Отсутствует ключ "homework_name" в домашней работе.')    
+        raise KeyError('Отсутствует ключ "homework_name" в домашней работе.')
     if homework_status not in HOMEWORK_VERDICTS:
         raise ValueError(f'Неизвестный статус: {homework_status}')
     verdict = HOMEWORK_VERDICTS.get(homework_status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-    
+
 
 def main():
-    """Основная логика работы бота."""    
+    """Основная логика работы бота."""
     if TELEGRAM_TOKEN is None:
         logging.critical(
             "Отсутствует переменная окружения: 'TELEGRAM_BOT_TOKEN'"
-            )
+        )
         return
     bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
@@ -130,7 +129,7 @@ def main():
                 message = parse_status(homework)
                 if message:
                     send_message(bot, message)
-            current_timestamp = int(time.time())
+            timestamp = int(time.time())
             time.sleep(RETRY_PERIOD)
 
         except Exception as error:
